cfe5c436c6ab19a2dbb8d5d7277af9e00ac5c043
z-michou <igoret@hotmail.com>
2015-05-27 12:01:07 -0700

    edit on branch 2

--- MySimplePythonApplication/Solver.py
+++ MySimplePythonApplication/Solver.py
@@ -7,9 +7,9 @@ class Solver:
         a = int(input('a '))
         b = int(input('b '))
         c = int(input('c '))
-        pre_dis = b**2 - 4*a*c
-        if pre_dis >= 0:
-            discreminant = math.sqrt(pre_dis)
+        d = b**2 - 4*a*c
+        if d >= 0:
+            discreminant = math.sqrt(d)
             root1 = (-b + discreminant) / (2*a)
             root2 = (-b - discreminant) / (2*a)
             print (root1, root2)
