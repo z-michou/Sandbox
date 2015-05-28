84cc6bcc53fc754fd9ffb596b835b0d7af41e964
z-michou <igoret@hotmail.com>
2015-05-27 18:23:05 -0700

    Show the main window without taking the input focus

--- SimpleQtExample/FirstTutorial.py
+++ SimpleQtExample/FirstTutorial.py
@@ -4,6 +4,15 @@ import sys
 from PySide.QtCore import *
 from PySide.QtGui import *
 
+def toggleOn():
+    label.show()
+    QTimer.singleShot(700, toggleOff)
+
+def toggleOff():
+    label.hide()
+    QTimer.singleShot(200, toggleOn)
+
+
 app = QApplication(sys.argv)
 
 label = QLabel('This is a message on a transparent label')
@@ -11,7 +20,8 @@ label = QLabel('This is a message on a transparent label')
 label.setStyleSheet('color: red; font: italic bold 30pt')
 label.setWindowFlags(Qt.FramelessWindowHint)
 label.setAttribute(Qt.WA_TranslucentBackground)
-label.show()
+label.setAttribute(Qt.WA_ShowWithoutActivating)
 
+toggleOn()
 QTimer.singleShot(5000, app.quit)
 app.exec_()
